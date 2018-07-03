9f442707b917dbe83eaeec491ff306f98660aaaf
Ashit Mundra <amundra6@gmail.com>
2018-07-03 17:04:26 +0530

    recent

--- recent.py
+++ recent.py
@@ -2,19 +2,21 @@
 import MySQLdb
 import os
 import datetime
+#database connection
 db= MySQLdb.connect(host="localhost", user="root", passwd="", db="major")
 cur = db.cursor()
-a=datetime.date.today()
-b=(a-datetime.timedelta(days=30))
-c=b.strftime("%y %m %d")
-d= c.split()
-e=int("20"+d[0])
-f=int(d[1])
-g=int(d[2])
-print(str(e)+", "+str(f)+", "+str(g))
-#x=datetime.date("int("+str(e)+"), int("+str(f)+"), int("+str(g)+")")
-#print(x)
-cur.execute("SELECT * from books_details WHERE publish_date>STR_TO_DATE('"+str(f)+", "+str(g)+", "+str(e)+"', '%m, %e, %Y')")
+#current date
+tday=datetime.date.today()
+#date 1 month ago
+pday=(tday-datetime.timedelta(days=30))
+final_pday=pday.strftime("%y %m %d")
+split_pday= final_pday.split()
+year="20"+split_pday[0]
+month=split_pday[1]
+date=split_pday[2]
+#extracting name of books uploaded in ecent month
+cur.execute("SELECT b_name from books_details WHERE publish_date>STR_TO_DATE('"+month+", "+date+", "+year+"', '%m, %e, %Y')")
 res=cur.fetchall()
+#printing books names
 print(res)
 
